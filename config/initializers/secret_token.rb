0932c9fe081e0c036b6d681916eb51ef53f32a5d
Ted Coleman <ted.coleman@sendgrid.com>
2013-11-29 13:50:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsTest::Application.config.secret_key_base = '3e68cad7f287ba001822576ccf0215227c104aa53f739319f4a2def640cb11c065d5da941028bb29de85c06ecb0bed710cfe8aa0179818aedacaa2e94ef24cdb'
